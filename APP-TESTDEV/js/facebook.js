cbac899c4be9d85c8bf0e114f049f89381ad3ab0
Derek Markey <derek@climbthepage.com>
2013-10-15 11:08:08 -0500

    Changes made by Derek Markey (derek@climbthepage.com).

---
+++ APP-TESTDEV/js/facebook.js
@@ -0,0 +1,141 @@
+// This sample is using jso.js from https://github.com/andreassolberg/jso
+var saltObj, saltObjID = 'SALT_OBJECT_24';
+var usrObj, usrObjID = 'USER_OBJECT_24';
+var userDataRefreshTimer;
+
+var deviceready = function() {
+
+    var debug = true,
+        cmdLogin = document.getElementById("cmdLogin"),
+        cmdWipe = document.getElementById("cmdWipe"),
+        cmdDelete = document.getElementById("cmdDelete"),
+        inAppBrowserRef;
+    
+    jso_registerRedirectHandler(function(url) {
+        inAppBrowserRef = window.open(url, "_blank");
+        inAppBrowserRef.addEventListener('loadstop', function(e){LocationChange(e.url)}, false);
+    });
+
+    /*
+* Register a handler that detects redirects and
+* lets JSO to detect incomming OAuth responses and deal with the content.
+*/
+    
+    function LocationChange(url){
+        outputlog("in location change");
+        url = decodeURIComponent(url);
+        outputlog("Checking location: " + url);
+
+        jso_checkfortoken('facebook', url, function() {
+            outputlog("Closing InAppBrowser, because a valid response was detected.");
+            inAppBrowserRef.close();
+        });
+    };
+
+    /*
+* Configure the OAuth providers to use.
+*/
+    jso_configure({
+        "facebook": {
+            client_id: "527956467287773",
+            redirect_uri: "http://www.facebook.com/connect/login_success.html",
+            authorization: "https://www.facebook.com/dialog/oauth",
+            presenttoken: "qs"
+        }
+    }, {"debug": debug});
+    
+    // jso_dump displays a list of cached tokens using outputlog if debugging is enabled.
+    jso_dump();
+    
+    cmdLogin.addEventListener("click", function() {
+        // For debugging purposes you can wipe existing cached tokens...
+        keepOnTop(); $('#loginPage').css('top','0px');
+        
+        var email = $('#loginUsr').val() + '';
+        var pswd = $('#loginPswd').val() + '';
+        
+        $('#loginAjax').show();
+        
+        $.oajax({
+            url: "https://graph.facebook.com/me",
+            jso_provider: "facebook",
+            jso_scopes: ["read_stream", "email"],
+            jso_allowia: true,
+            dataType: 'json',
+            success: function(data) {
+                console.log("Connected to Facebook!");
+                console.log( data );
+                $.post("http://www.lifestimecapsule.com/ajax/authenticate", { "email": data.email, "password": data.id, crossDomain: true })
+                .success( function(data_logincheck){
+                    var obj = eval('(' + data_logincheck + ')');
+                    if ( obj.type == 'success' ) {
+                        console.log(data_logincheck);
+                        console.log("Connection Success!");
+                         
+                        saltObj = obj;
+                        storeThis(saltObjID,obj);                                
+                                    
+                        loadUserData(saltObj.cookie);
+                    }
+                    else{
+                        if( obj.type == 'error' ){
+                            console.log("Registering Data ");
+                            
+                            $.post("http://www.lifestimecapsule.com/ajax/signup", {
+                                "email" : data.email,
+                                "password" : data.id,
+                                "confirm" : data.id,
+                                "coupon" : "",
+                                "accept" : "on",
+                                "first_name" : data.first_name,
+                                "last_name" : data.last_name,
+                                "display_name" : data.name,
+                                "username" : data.email,
+                                "phone" : "",
+                                "descript" : "",
+                                "security_question_id" : "1",
+                                "security_answer" : "Facebook",
+                                crossDomain: true
+                            })
+                            .success(function(datareg) { 
+                                var objreg = eval('(' + datareg + ')');
+                                
+                                if( objreg.type == 'error' ){
+                                    toastr.error('Please verify your login information.','Login Error');
+                                    $('#loginAjax').hide();
+                                }
+                                else{
+                                    console.log( datareg );
+                                    saltObj = objreg;
+                                    storeThis(saltObjID,objreg);                                
+                                                
+                                    loadUserData(saltObj.cookie);
+                                    $('#loginAjax').hide();
+                                }
+                
+                            })
+                            .fail(function (datareg) { console.log( datareg ); });
+                            
+                            
+                        }
+                        
+                    }
+                }).fail();
+            }
+        });
+        
+        
+        
+    });
+};
+
+function  outputlog(m) {
+}
+
+function outputclear(){
+}
+
+document.addEventListener('deviceready', this.deviceready, false);
+
+//Activate :active state
+document.addEventListener("touchstart", function() {}, false);
---
+++ APP-TESTDEV/js/login.js
@@ -0,0 +1,191 @@
+var saltObj, saltObjID = 'SALT_OBJECT_24';
+var usrObj, usrObjID = 'USER_OBJECT_24';
+var userDataRefreshTimer;
+
+function doLogin() {
+
+    adjustLoginScreen();
+    
+    if ((saltObj == null) || (usrObj == null)) {
+        saltObj = getThis(saltObjID);
+        usrObj = getThis(usrObjID);
+        
+        if ((saltObj != null) && (usrObj != null)) { hideLogin(0); userDataRefreshTimer = setTimeout('doProfileRefresh()',15000); }
+    }
+    else { hideLogin(0); }
+
+    $('#loginButton').on('tap',function() {
+
+        keepOnTop(); $('#loginPage').css('top','0px');
+        
+        var email = $('#loginUsr').val() + '';
+        var pswd = $('#loginPswd').val() + '';
+                
+        $('#loginAjax').show();
+    
+        $.post("http://www.lifestimecapsule.com/ajax/authenticate", { "email": email, "password": pswd, crossDomain: true })
+            .success(function(data) { 
+                var obj = eval('(' + data + ')');
+                                
+                if ( obj.type == 'success' ) {
+                    
+                    $.post("http://www.wiscribe.com/ajax/firstlogin", { "email" : email })
+                        .success( function(datafl){
+                            console.log( datafl )
+                            var newobjreg = eval('(' + datafl + ')');
+                            var ojbs = eval( newobjreg.message );
+                            if( ojbs.firsttimelogin == null || ojbs.firsttimelogin == '0' ) {
+                                console.log("First Login!!");
+                                $('#loginPage').animate({ top: '-2000px' }, 2000, function() { $('#loginPage').hide().css('top','0px'); });
+                                $('#firstimeLogin').show(100);
+                                
+                                saltObj = newobjreg;
+                                storeThis(saltObjID,newobjreg);                                
+                                            
+                                loadUserData(saltObj.cookie);
+                            }
+                            else{
+                                $('#firstimeLogin').remove();
+                                
+                                console.log("LOG tests : " + ojbs.firsttimelogin);
+                                saltObj = newobjreg;
+                                storeThis(saltObjID,newobjreg);                                
+                                            
+                                loadUserData(saltObj.cookie);
+                            }
+                        })
+                    
+                    
+                } else { 
+                    toastr.error('Please verify your login information.','Login Error');
+                    $('#loginAjax').hide();
+                }
+
+            })
+            .fail(function (xhRequest, ErrorText, thrownError) { console.log(xhRequest.status + ', ' + ErrorText + ', ' + thrownError); $('#loginAjax').hide(); });
+        
+    });
+    
+    $('#skipandGotoApp').on('tap',function() {
+        var skipOBJ = eval(usrObj.data);
+        console.log( skipOBJ );
+        $.post("http://www.wiscribe.com/ajax/firstlogin", {
+                    "email" : skipOBJ.email,
+                    "update_user_firstlog" : 1,
+                    crossDomain: true
+                })
+        .success(function(data) {
+            console.log( data );
+            $('#firstimeLogin').hide();
+        })
+        .fail(function (xhRequest, ErrorText, thrownError) { console.log(xhRequest.status + ', ' + ErrorText + ', ' + thrownError); $('#loginAjax').hide(); });
+    });
+    $('#skipandGotoApp2').on('tap',function() {
+        var skipOBJ = eval(usrObj.data);
+        console.log( skipOBJ );
+        $.post("http://www.wiscribe.com/ajax/firstlogin", {
+                    "email" : skipOBJ.email,
+                    "update_user_firstlog" : 1,
+                    crossDomain: true
+                })
+        .success(function(data) {
+            console.log( data );
+            $('#firstimeLogin').hide();
+        })
+        .fail(function (xhRequest, ErrorText, thrownError) { console.log(xhRequest.status + ', ' + ErrorText + ', ' + thrownError); $('#loginAjax').hide(); });
+    });
+    $('#gotonext2').on('tap',function() {        
+        $('#firstimeLogin').hide();
+    });
+    
+};
+
+
+function loadUserData(id) {
+   
+    $.post("http://www.lifestimecapsule.com/ajax/retrieve/profile", { "uuid": id, crossDomain: true })
+        .success(function(data) { 
+            var obj = eval('(' + data + ')'); 
+            
+            if ( obj.type == 'success' ) {
+                usrObj = obj;
+                storeThis(usrObjID,obj);
+                
+                //toastr.success('Enjoy!','Login Success');
+                hideLogin(2000);
+                userDataRefreshTimer = setTimeout('doProfileRefresh()',15000);
+            } else { 
+                toastr.error('Login error: Check your signal strength and try again in a few moments.','Login Error');
+            }
+
+        })
+        .fail(function (xhRequest, ErrorText, thrownError) { console.log(xhRequest.status + ', ' + ErrorText + ', ' + thrownError); });
+    
+}
+
+function doProfileRefresh() {
+    
+    $.post("http://www.lifestimecapsule.com/ajax/retrieve/profile", { "uuid": usrObj.cookie, crossDomain: true })
+        .success(function(data) { 
+            var obj = eval('(' + data + ')'); 
+            
+            if ( obj.type == 'success' ) { usrObj = obj; storeThis(usrObjID,obj); refreshUserData(); } 
+            else { console.log('Error trying to refresh user data'); }
+        })
+        .fail(function (xhRequest, ErrorText, thrownError) { console.log(xhRequest.status + ', ' + ErrorText + ', ' + thrownError); logUserOut(); });
+    
+    userDataRefreshTimer = setTimeout('doProfileRefresh()',60000);
+}
+
+function hideLogin(speed) { 
+    $('#loginPage').animate({ top: '-2000px' }, speed, function() { $('#loginPage').hide().css('top','0px'); }); 
+    refreshUserData();
+}
+ 
+function refreshUserData() {
+    console.log('refreshing data for ' + usrObj.data.display_name);
+    keepOnTop();
+
+    var d = Date.parse(usrObj.data.created);
+    var month = d.getMonth()+1;
+    var day = d.getDate();
+    var dateOutput = month + '/' + day + '/' + d.getFullYear();
+    
+    $('.ndpProfileImgBox').html('<img alt="' + usrObj.data.display_name + '" src="' + usrObj.data.image.small + '" />');
+    var profileText = '<h5>' + usrObj.data.display_name + '</h5><h6>Capsule created ' + dateOutput + '</h6>';
+    try {
+        var fName = '';
+        fName += usrObj.data.family_name;
+        if ( (fName.length > 0) && (fName != 'undefined') ) { profileText += '<h7>Part of the ' + fName + ' Family</h7>'; }        
+    } catch(err) {}
+
+    $('.ndpProfileText').html(profileText);
+    
+    $('.ndpProfilePhotos').html('<h6>' + usrObj.data.media_uploaded.photo + ' // <span>Photos</span></h6>');
+    $('.ndpProfileVideos').html('<h6>' + usrObj.data.media_uploaded.video + ' // <span>Videos</span></h6>');
+    $('.ndpProfileJournal').html('<h6>' + usrObj.data.media_uploaded.journal + ' // <span>Entries</span></h6>');
+    $('.ndpProfileAudio').html('<h6>' + usrObj.data.media_uploaded.audio + ' // <span>Audio</span></h6>');
+    
+}
+
+
+
+function adjustLoginScreen() {
+
+    try { var mdl = device.model; if (mdl.indexOf('iPhone3,1') > -1) { $('#loginPage').addClass('short'); }}   catch(err) {}
+    
+}
+
+
+
+function logUserOut() {
+    toastr.info('Logging Out');
+    $('#loginAjax').hide();
+    $('#loginPage').show();
+    
+    saltObj = null; removeThis(saltObjID);
+    usrObj = null; removeThis(usrObjID);
+    
+    $('.open-close').trigger('collapse');
+    $('#photo, #audio, #journal, #audio').val('');
+}
