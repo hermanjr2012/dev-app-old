32159a0e8e22e918e3812f7101346d01b0419a2f
Derek Markey <derek@climbthepage.com>
2013-11-25 14:41:19 -0600

    Changes made by Neo

--- LTC-1.5/js/audio.js
+++ LTC-1.5/js/audio.js
@@ -67,7 +67,11 @@ audioApp.prototype={
             $('#audio-add').addClass('noPreview');
         } else {
             $('#audioArea #audioContainer').html('<audio controls style="width: 300px;"><source src="' + that._captureArray[0].fullPath + '"></audio>');
-            $('#audioDuration').text("Duration: <strong>" + that._captureArray[0].duration + "</strong>");
+            var formatSuccess = function (capturedFiles) {
+                $('#audioDuration').text("Duration: <strong>" + capturedFiles.duration + "</strong>");
+            };
+            
+            
         }
         
         
