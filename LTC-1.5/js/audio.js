c53321611a78f7cccc15df69acdfbfaab6e8f52b
Derek Markey <derek@climbthepage.com>
2013-11-26 13:02:34 -0600

    Changes made by Derek Markey (derek@climbthepage.com).

--- LTC-1.5/js/audio.js
+++ LTC-1.5/js/audio.js
@@ -69,8 +69,9 @@ audioApp.prototype={
             $('#audio-add').addClass('noPreview');
         } else {
             var my_media = new Media(audioFile);
+            var dur = my_media.getDuration();
             $('#audioArea #audioContainer').html('<audio controls style="width: 300px;"><source src="' + that._captureArray[0].fullPath + '"></audio>');
-            $('#audioArea #audioDuration').append("Duration: " + my_media.getDuration());
+            $('#audioArea #audioDuration').append("Duration: " + dur);
             
             
             
