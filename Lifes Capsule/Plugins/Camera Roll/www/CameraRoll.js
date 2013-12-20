19f157352dc1f6371a30856225bda604df958def
Derek Markey <derek@climbthepage.com>
2013-12-20 06:25:18 -0600

    Changes made by Derek Markey (derek@climbthepage.com).

--- Lifes Capsule/Plugins/Camera Roll/www/CameraRoll.js
+++
@@ -1,9 +0,0 @@
-var exec = require('cordova/exec');
-
-var cameraRoll = {};
-
-cameraRoll.getPhotos = function(successCallback, errorCallback, options) {
-  exec(successCallback, errorCallback, "CameraRoll", "getPhotos", []);
-};
-
-module.exports = cameraRoll;
