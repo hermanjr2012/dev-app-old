b549ef1607296318d363b0f490a11ef90c2af47e
Derek Markey <derek@climbthepage.com>
2013-12-20 06:49:37 -0600

    Changes made by Derek Markey (derek@climbthepage.com).

--- Lifes Capsule/js/photolibrary.js
+++ Lifes Capsule/js/photolibrary.js
@@ -1,1 +1,26 @@
-﻿
+var getPhotoLibrary = {
+    init: function(){
+        
+    },
+    
+    displayPhotoFromCamRoll: function() {
+      var picSource = navigator.camera.PictureSourceType;
+      picSource.PHOTOLIBRARY;
+      //CameraRoll.getPhotos(this.showGallery,this.getPhotosURIFail); 
+      CameraRoll.getPhotos(this.showGallery,this.getPhotosURIFail);          
+    },
+    
+    showGallery: function(pic) {
+      alert('pic');
+    },
+    
+    getPhotosURIFail: function() {
+      //todo
+    }
+};  
+
+var CameraRoll = {
+    getPhotos: function(cb, cf){
+        
+    }
+} 
